65a744c830a92401822bf26a44edd5d6b003cc67
Antonio Rodrigues <antoniorfaria89@gmail.com>
2025-12-24 18:11:54 -0300

    Fix app.py for Render deploy

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, send_from_directory
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,12 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-# Rota GLOBAL: Ela serve QUALQUER arquivo dentro da pasta static e suas subpastas
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    # Isso permite carregar: /static/img, /static/certificados, /static/logofacul, etc.
-    return send_from_directory(os.path.join(app.root_path, 'static'), filename)
-
 if __name__ == "__main__":
-    # Mantendo suas configurações de host e porta
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run()
